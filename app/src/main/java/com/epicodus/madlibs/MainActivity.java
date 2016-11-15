000e47da57eef983baaaa3039e1a35730a23789a
Stephen Emery and Sheena Nickerson <stephen@>
2016-11-15 10:37:11 -0800

    Refactor words into an arraylist.

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -7,18 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.createButton) Button mCreateButton;
+    @Bind(R.id.word0EditText) EditText mWord0EditText;
     @Bind(R.id.word1EditText) EditText mWord1EditText;
     @Bind(R.id.word2EditText) EditText mWord2EditText;
     @Bind(R.id.word3EditText) EditText mWord3EditText;
     @Bind(R.id.word4EditText) EditText mWord4EditText;
     @Bind(R.id.word5EditText) EditText mWord5EditText;
     @Bind(R.id.word6EditText) EditText mWord6EditText;
-    @Bind(R.id.word7EditText) EditText mWord7EditText;
+    ArrayList<String> words = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +35,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v == mCreateButton) {
-            String word1 = mWord1EditText.getText().toString();
-            String word2 = mWord2EditText.getText().toString();
-            String word3 = mWord3EditText.getText().toString();
-            String word4 = mWord4EditText.getText().toString();
-            String word5 = mWord5EditText.getText().toString();
-            String word6 = mWord6EditText.getText().toString();
-            String word7 = mWord7EditText.getText().toString();
+            words.add(mWord0EditText.getText().toString());
+            words.add(mWord1EditText.getText().toString());
+            words.add(mWord2EditText.getText().toString());
+            words.add(mWord3EditText.getText().toString());
+            words.add(mWord4EditText.getText().toString());
+            words.add(mWord5EditText.getText().toString());
+            words.add(mWord6EditText.getText().toString());
             Intent intent = new Intent(MainActivity.this, StoryActivity.class);
-            intent.putExtra("word1", word1);
-            intent.putExtra("word2", word2);
-            intent.putExtra("word3", word3);
-            intent.putExtra("word4", word4);
-            intent.putExtra("word5", word5);
-            intent.putExtra("word6", word6);
-            intent.putExtra("word7", word7);
+            intent.putExtra("words", words);
             startActivity(intent);
         }
 
--- app/src/main/java/com/epicodus/madlibs/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -18,16 +20,10 @@ public class StoryActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
-        String word1 = intent.getStringExtra("word1");
-        String word2 = intent.getStringExtra("word2");
-        String word3 = intent.getStringExtra("word3");
-        String word4 = intent.getStringExtra("word4");
-        String word5 = intent.getStringExtra("word5");
-        String word6 = intent.getStringExtra("word6");
-        String word7 = intent.getStringExtra("word7");
-        String text = "Last month, I went to Disney World with " + word1 + ". We traveled for " + word2 + " by " + word3 + ". " +
-                "Finally, we got there and it was very " + word4 + ". There were " + word5 + " people " + word6 +
-                " everywhere. There were also people dressed up in " + word7 + " costumes.";
+        ArrayList words = intent.getStringArrayListExtra("words");
+        String text = "Last month, I went to Disney World with " + words.get(0) + ". We traveled for " + words.get(1) + " by " + words.get(2) + ". " +
+                "Finally, we got there and it was very " + words.get(3) + ". There were " + words.get(4) + " people " + words.get(5) +
+                " everywhere. There were also people dressed up in " + words.get(6) + " costumes.";
         mStoryTextView.setText(text);
     }
 }
