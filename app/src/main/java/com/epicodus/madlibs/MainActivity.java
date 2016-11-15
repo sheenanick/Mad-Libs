fe240d17e892c7317f4c4fc0826cd312a2412c91
Stephen Emery and Sheena Nickerson <sheena@>
2016-11-15 09:58:44 -0800

    send words through intent to story activity

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -5,12 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.createButton) Button mCreateButton;
+    @Bind(R.id.word1EditText) EditText mWord1EditText;
+    @Bind(R.id.word2EditText) EditText mWord2EditText;
+    @Bind(R.id.word3EditText) EditText mWord3EditText;
+    @Bind(R.id.word4EditText) EditText mWord4EditText;
+    @Bind(R.id.word5EditText) EditText mWord5EditText;
+    @Bind(R.id.word6EditText) EditText mWord6EditText;
+    @Bind(R.id.word7EditText) EditText mWord7EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
-        startActivity(intent);
+        if (v == mCreateButton) {
+            String word1 = mWord1EditText.getText().toString();
+            String word2 = mWord2EditText.getText().toString();
+            String word3 = mWord3EditText.getText().toString();
+            String word4 = mWord4EditText.getText().toString();
+            String word5 = mWord5EditText.getText().toString();
+            String word6 = mWord6EditText.getText().toString();
+            String word7 = mWord7EditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+            intent.putExtra("word1", word1);
+            intent.putExtra("word2", word2);
+            intent.putExtra("word3", word3);
+            intent.putExtra("word4", word4);
+            intent.putExtra("word5", word5);
+            intent.putExtra("word6", word6);
+            intent.putExtra("word7", word7);
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/com/epicodus/madlibs/StoryActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryActivity.java
@@ -3,14 +3,28 @@ package com.epicodus.madlibs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StoryActivity extends AppCompatActivity {
+    @Bind(R.id.storyTextView) TextView mStoryTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
+        String word1 = intent.getStringExtra("word1");
+        String word2 = intent.getStringExtra("word2");
+        String word3 = intent.getStringExtra("word3");
+        String word4 = intent.getStringExtra("word4");
+        String word5 = intent.getStringExtra("word5");
+        String word6 = intent.getStringExtra("word6");
+        String word7 = intent.getStringExtra("word7");
+        mStoryTextView.setText(word1 + word2);
     }
 }
